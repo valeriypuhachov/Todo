07f36f378a4912812eab2f2c07f95fd1c35f418c
Valeriy Pugachov <valerii.puhachov@hneu.net>
2016-12-16 01:56:15 +0200

    Preloader was added.

---
+++ Todo.WebUI/Scripts/Preloader.js
@@ -0,0 +1,7 @@
+﻿$.ready(function ($) {
+    $(window).load(function () {
+        setTimeout(function () {
+            $('.pl').fadeOut('slow', function () { });
+        }, 2000);
+    });
+});
--- Todo.WebUI/Scripts/_references.js
+++ Todo.WebUI/Scripts/_references.js
@@ -10,5 +10,6 @@
 /// <reference path="jquery-3.1.1.slim.min.js" />
 /// <reference path="map.js" />
 /// <reference path="modernizr-2.8.3.js" />
+/// <reference path="preloader.js" />
 /// <reference path="respond.matchmedia.addlistener.min.js" />
 /// <reference path="respond.min.js" />
